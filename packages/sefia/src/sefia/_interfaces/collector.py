8dcfe6e79b31b5027b25f05f0e1047f7eaa58b01
nueruyu <81212943+nueruyu@users.noreply.github.com>
2026-06-12 19:17:56 +0900

    Remove private collector interface

--- packages/sefia/src/sefia/_interfaces/collector.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from .._tool_registry import ToolRegistry
-
-
-class ToolCollector(ABC):
-    @abstractmethod
-    def collect(self, instance: object) -> ToolRegistry:
-        ...
