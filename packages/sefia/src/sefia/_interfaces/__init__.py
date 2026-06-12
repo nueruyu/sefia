f10d50d22cfcc13f81e02e0e64918c967d43df26
nueruyu <81212943+nueruyu@users.noreply.github.com>
2026-06-12 12:36:55 +0900

    Rename interfaces package as private

---
+++ packages/sefia/src/sefia/_interfaces/__init__.py
@@ -0,0 +1,29 @@
+from .event_handler import EventHandler
+from .inference_strategy import InferenceStrategy
+from .middleware import (
+    InferenceMiddleware,
+    InferenceContext,
+    StepContext,
+    StepMiddleware,
+)
+from .model_inspector import ModelInspector
+from .policy import Policy
+from .prompt_formatter import PromptFormatter
+from .resource import Resource
+from .session_store import SessionStore
+from .tool_collector import ToolCollector
+
+__all__ = [
+    "EventHandler",
+    "InferenceStrategy",
+    "InferenceMiddleware",
+    "StepMiddleware",
+    "InferenceContext",
+    "StepContext",
+    "ModelInspector",
+    "Policy",
+    "PromptFormatter",
+    "Resource",
+    "SessionStore",
+    "ToolCollector",
+]
