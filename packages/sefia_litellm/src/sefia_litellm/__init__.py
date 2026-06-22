9af283532c2c982b2f5cca593431d22c5bf5204f
nueruyu <81212943+nueruyu@users.noreply.github.com>
2026-06-22 21:25:37 +0900

    refactor

--- packages/sefia_litellm/src/sefia_litellm/__init__.py
+++ packages/sefia_litellm/src/sefia_litellm/__init__.py
@@ -1,15 +1,3 @@
 from ._client import LiteLLMClient
-from .exceptions import (
-    InferenceConnectionError,
-    InferenceRateLimitError,
-    InferenceTemporarilyUnavailableError,
-    InferenceTimeoutError,
-)
 
-__all__ = [
-    "LiteLLMClient",
-    "InferenceTimeoutError",
-    "InferenceConnectionError",
-    "InferenceRateLimitError",
-    "InferenceTemporarilyUnavailableError",
-]
+__all__ = ["LiteLLMClient"]
